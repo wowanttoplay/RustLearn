754a5ad40d8cf241dffd3e58bd4ffce1f15f3a02
范杰 <49019380+wowanttoplay@users.noreply.github.com>
2024-11-17 19:59:55 +0800

    Add user input-based array access and update project config, have learned the basic data type, const and shadow logic

--- basic_program/src/main.rs
+++ basic_program/src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
+const THREE_MONTH_IN_SECONDS: u32 = 60 * 60 * 3;
 fn main() {
-    println!("Hello, world!");
+    println!("{}", THREE_MONTH_IN_SECONDS);
+    let arr = [1,2,3,4,5,6,7,8,9,10];
+
+    loop {
+        let mut index = String::new();
+        io::stdin()
+            .read_line(&mut index)
+            .expect("Failed to read line");
+
+        let index: usize = index.trim().parse().expect("Please type a number!");
+
+        println!("{}", arr[index]);
+    }
 }
