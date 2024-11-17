48e8785b2a264af6dcdfceafeb7d7dbf9aa024bd
范杰 <49019380+wowanttoplay@users.noreply.github.com>
2024-11-17 19:19:39 +0800

    Refactor looping logic for guessing input
    Moved the input prompt and guess handling into a loop to allow multiple attempts. The loop now breaks upon a correct guess, ensuring continuous play until a win. Adjusted import positioning for code clarity.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -9,21 +9,27 @@ fn main() {
 
     println!("The secret number is {}", secret_number);
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+
+            Ordering::Greater => println!("Too big!"),
+        }
     }
 }
