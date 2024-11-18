9c6650ff7e73c43e2c7d1f7e15516cd0fc562141
DESKTOP-14Q5NUC\vir <948420291@qq.com>
2024-11-18 11:56:35 +0800

    Add fn and know the difference between statement and expression

--- other_function/src/main.rs
+++ other_function/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
     println!("Hello, world!");
+
+    another_function();
+
+    another_function2(5);
+
+    print_labeled_measurement(1, 'b');
+
+    let y = {
+        let x = 1; // This is a statement, not an expression
+        println!("The value of x is: {}", x);
+        x + 1
+    }; // The curly brackets is an expression, so x + 1 is not end with semicolon. let y = {} is a statement
+
+    println!("y is {}", y);
+}
+
+fn another_function() {
+    println!("Another function");
+}
+
+fn another_function2(x: i32) {
+    println!("The value of x is: {}", x);
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {}{}", value, unit_label);
+    println!("The measurement is {value}{unit_label}");
 }
